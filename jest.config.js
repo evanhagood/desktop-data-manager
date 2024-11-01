21ff7251471739643fed4dea8e98a897bd69aa6e
evanhagood <evanmhagood@gmail.com>
2024-11-01 19:24:38 -0400

    Update jest to adequately deal with CSS imports

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
-// jest.config.cjs
-export default {
+// jest.config.js
+module.exports = {
     testEnvironment: "jest-environment-jsdom",
     transform: {
-      "^.+\\.jsx?$": "babel-jest",
+        "^.+\\.jsx?$": "babel-jest",
     },
     extensionsToTreatAsEsm: [".jsx"],
-  };
-  
+    moduleNameMapper: {
+        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    },
+};
